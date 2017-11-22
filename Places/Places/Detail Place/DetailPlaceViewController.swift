02f3d1b530bd965168ce28b7becc4e204e55ec70
Andrii Antoniak <andry.br98@gmail.com>
2017-11-22 13:12:59 +0200

    add detail place storyboard

---
+++ Places/Places/Detail Place/DetailPlaceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailPlaceViewController.swift
+//  Places
+//
+//  Created by Andrii Antoniak on 11/22/17.
+//  Copyright © 2017 andriibilan. All rights reserved.
+//
+
+import UIKit
+
+class DetailPlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
