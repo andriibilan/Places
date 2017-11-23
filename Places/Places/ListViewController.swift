be203f5029a14a8ff5150fdebd7b0e03cc8cf1d3
andriibilan <32512002+andriibilan@users.noreply.github.com>
2017-11-23 16:42:10 +0200

    add outlet for listViewController

--- Places/Places/ListViewController.swift
+++ Places/Places/ListViewController.swift
@@ -9,27 +9,27 @@
 import UIKit
 
 class ListViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 0
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let listCell = tableView.dequeueReusableCell(withIdentifier: "listTableView", for: indexPath) as! ListTableViewCell
+//
+//        return listCell
+//    }
 }
