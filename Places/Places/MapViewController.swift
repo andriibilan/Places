c1a9725a7f0660e3e7407cf4f1c8a5c813311e89
Andrew <andrew@macminissu9.training.local>
2017-11-24 13:45:49 +0200

    Long press

--- Places/Places/MapViewController.swift
+++ Places/Places/MapViewController.swift
@@ -77,6 +77,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         // Do any additional setup after loading the view.
     }
 
+    // radius / places in radius
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     {
