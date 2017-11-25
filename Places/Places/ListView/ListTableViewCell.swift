f76806b16c8ec6b2aef1d5ba8f1f065903e7474f
Roman Melnychok <roman.melnychok@gmail.com>
2017-11-25 21:58:51 +0200

    add class PlaceTemp, filling list with data

--- Places/Places/ListView/ListTableViewCell.swift
+++ Places/Places/ListView/ListTableViewCell.swift
@@ -8,23 +8,31 @@
 
 import UIKit
 
+
 class ListTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var imageIcon: UIImageView!
-    @IBOutlet weak var namePlace: UILabel!
-    @IBOutlet weak var isOpen: UILabel!
-    @IBOutlet weak var distanceToUser: UILabel!
-    
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
-
+	
+	@IBOutlet weak var imageIcon: UIImageView!
+	@IBOutlet weak var namePlace: UILabel!
+	@IBOutlet weak var isOpen: UILabel!
+	@IBOutlet weak var distanceToUser: UILabel!
+	
+	
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		// Initialization code
+	}
+	
+	
+	
+	func configureCell(with place: PlaceTemp)  {
+		
+		imageIcon.image = #imageLiteral(resourceName: "noPhotoIcon")
+		namePlace.text = "temp"
+		isOpen.text = "open"
+		isOpen.textColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
+		
+	}
+	
 }
+
