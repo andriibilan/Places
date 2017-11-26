d38164c33cfadc80a0aa8c85662ed378c23190e4
Yurii Vients <yuriivients@Yuriis-MacBook-Pro.local>
2017-11-26 14:48:08 +0200

    create User model, fixed create user

--- Places/Places/AppDelegate.swift
+++ Places/Places/AppDelegate.swift
@@ -18,6 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         return true
+    }
+    
+    func showProfile(){
         
+        if Auth.auth().currentUser != nil {
+            let profileVC = UIStoryboard(name: "Profile", bundle: nil).instantiateViewController(withIdentifier: "ProfileVC") as! ProfileViewController
+            self.window?.rootViewController = profileVC
+            
+        }
     }
 }
---
+++ Places/Places/Login/Validator.swift
@@ -0,0 +1,29 @@
+//
+//  Validator.swift
+//  Places
+//
+//  Created by Yurii Vients on 11/23/17.
+//  Copyright © 2017 andriibilan. All rights reserved.
+//
+
+import Foundation
+
+class Validator {
+    
+    func isValidEmail(email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let pred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return pred.evaluate(with: email)
+    }
+    
+    func isValidPassword(password: String) -> Bool {
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", ".{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+    func isValidPhoneNumber(testStr: String) -> Bool {
+        let phoneRegEx = "^\\d{3}-\\d{3}-\\d{4}$"
+        let phoneTest = NSPredicate(format:"SELF MATCHES %@", phoneRegEx)
+        return phoneTest.evaluate(with: testStr)
+    }
+}
