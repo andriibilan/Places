0fcad2898b02c71fe7c4c518a412635e73012ab8
Yurii Vients <yuriivients@Yuriis-MacBook-Pro.local>
2017-11-28 21:39:31 +0200

    commit

--- Places/Places/ViewController.swift
+++ Places/Places/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
     
-    
-    
     @IBOutlet weak var mapView: UIView!
     @IBOutlet weak var listView: UIView!
     @IBOutlet weak var menuView: UIViewX!
