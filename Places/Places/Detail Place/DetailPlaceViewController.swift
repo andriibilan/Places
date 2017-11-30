ca5d06a776773d41670ce7446586be5fdf63077b
trori46 <vrohozhyna@gmail.com>
2017-11-30 13:03:02 +0200

    no message

--- Places/Places/Detail Place/DetailPlaceViewController.swift
+++ Places/Places/Detail Place/DetailPlaceViewController.swift
@@ -62,7 +62,7 @@ class DetailPlaceViewController: UIViewController, UICollectionViewDelegate, UIC
         
         placeHours?.text = testPlace.hours
         
-        placePhone?.text = testPlace.phoneNumber
+        placePhone?.text = testPlace.phone
         //
         
         
