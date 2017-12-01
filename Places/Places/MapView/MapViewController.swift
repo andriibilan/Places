ad1e86330d7631f7ec1feee03cc314b462e3e098
trori46 <vrohozhyna@gmail.com>
2017-12-01 14:57:34 +0200

    no message

--- Places/Places/MapView/MapViewController.swift
+++ Places/Places/MapView/MapViewController.swift
@@ -363,6 +363,11 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         if control == view.rightCalloutAccessoryView || control == view.detailCalloutAccessoryView || control == view.leftCalloutAccessoryView {
             //Perform a segue here to navigate to another viewcontroller
             let g = view.annotation as! CustomAnnotation
+            
+            
+            
+            
+            
             performSegue(withIdentifier: "detailVC", sender: g.place)
         }
     }
@@ -386,7 +391,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         /////////////////////////////////////////////////////////////////////////
         
         let nameFilterArray = [ "Bar","Cafe","Restaurant", "Bank","Night Club","Museum", "Beuty Salon","Pharmacy","Hospital","Bus Station","Gas Station","University","Police","Church","Cemetery","Park","Gym"]
-        let iconFilterArray = [#imageLiteral(resourceName: "bar"),#imageLiteral(resourceName: "cafe"),#imageLiteral(resourceName: "restaurant"), #imageLiteral(resourceName: "bank"),#imageLiteral(resourceName: "nightClub") ,#imageLiteral(resourceName: "museum"),#imageLiteral(resourceName: "beutySalon"),#imageLiteral(resourceName: "pharmacy"),#imageLiteral(resourceName: "hospital"),#imageLiteral(resourceName: "busStation"),#imageLiteral(resourceName: "gasStation"),#imageLiteral(resourceName: "university"), #imageLiteral(resourceName: "police"),#imageLiteral(resourceName: "church"),#imageLiteral(resourceName: "cemetery"),#imageLiteral(resourceName: "park"),#imageLiteral(resourceName: "gym")]
+        let iconFilterArray = [#imageLiteral(resourceName: "bar"),#imageLiteral(resourceName: "cafe"),#imageLiteral(resourceName: "restaurant"), #imageLiteral(resourceName: "bank"),#imageLiteral(resourceName: "nightClub") ,#imageLiteral(resourceName: "museum"),#imageLiteral(resourceName: "beutySalon"),#imageLiteral(resourceName: "pharmacy"),#imageLiteral(resourceName: "hospital"),#imageLiteral(resourceName: "busStation"),#imageLiteral(resourceName: "gasStation"),#imageLiteral(resourceName: "university"), #imageLiteral(resourceName: "police"),#imageLiteral(resourceName: "Church"),#imageLiteral(resourceName: "cemetery"),#imageLiteral(resourceName: "park"),#imageLiteral(resourceName: "gym")]
 
      func numberOfSections(in tableView: UITableView) -> Int {
             return 1
--- Places/Places/MapView/myAnnotation.swift
+++ Places/Places/MapView/myAnnotation.swift
@@ -24,7 +24,7 @@ class CustomAnnotation : NSObject, MKAnnotation {
         self.place = place
         self.title = place.name
         self.isOpen = place.isOpen
-        self.image = place.icon
+        self.image = (place.icon)?.resizedImage(withBounds: CGSize(width: 40.0, height: 40.0))
         if place.isOpen == true{
             subtitle = "Open now"
         } else {
