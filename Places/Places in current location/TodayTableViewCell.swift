54642bd9cfb854440573e3444c5ba74f7077d19d
Andrew <andrew@macminissu9.training.local>
2017-12-06 19:07:44 +0200

    Convert to celsius in widget

--- Places/Places in current location/TodayTableViewCell.swift
+++ Places/Places in current location/TodayTableViewCell.swift
@@ -2,14 +2,14 @@
 //  TodayTableViewCell.swift
 //  Places In Current Location
 //
-//  Created by Andrew on 11/30/17.
+//  Created by Andrew Konchak on 11/30/17.
 //  Copyright © 2017 andriibilan. All rights reserved.
 //
 
 import UIKit
 
 class TodayTableViewCell: UITableViewCell {
-
+    
     
     @IBOutlet weak var placeImage: UIImageView!
     @IBOutlet weak var placeName: UILabel!
@@ -20,11 +20,11 @@ class TodayTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
